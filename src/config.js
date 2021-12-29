b467de0a2d3c339692b3d9e1a2a65ea1a0c88b9a
Gustavo A <gustavo.atencio@outlook.com>
2021-12-28 22:38:19 -0500

    Using enviromental variables

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export const VITE_DRIVE_CLIENT_ID = import.meta.env.VITE_DRIVE_CLIENT_ID
+export const VITE_DRIVE_CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET
+
+export default {
+  VITE_DRIVE_CLIENT_SECRET,
+  VITE_DRIVE_CLIENT_ID
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
+import env from "./config"
+
+console.log(env)
 
 ReactDOM.render(
   <React.StrictMode>
